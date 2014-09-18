da36a9fcc23d6d7edb69daaebec4fed0088c0db5
reckbo <reckbo@bwh.harvard.edu>
2014-09-17 23:44:18 -0400

    ENH/BUG: statush.sh: Add -s option, make work with multiple $case's

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@ set -eu
 
 HELP_TEXT="Usage:
         
-    $(basename $0) <CASELIST> <filepattern1> <filepattern2> ... <filepatternN>
+    $(basename $0) [-s] <CASELIST> <filepattern1> <filepattern2> ... <filepatternN>
 
 Prints a summary of which files exist (e.g. generated) and which ones remain to
 be computed for each case in the <CASELIST> text file.
@@ -20,7 +20,7 @@ completed() {
     local case=$1 && shift
     local filepatterns="$@"
     for filepattern in $filepatterns; do
-        [ ! -f ${filepattern/\$case/$case} ] && return 1
+        [ ! -f ${filepattern//\$case/$case} ] && return 1
     done
     return 0
 }
@@ -29,23 +29,24 @@ ratio_percent() {
     echo "scale=1; ($1 * 100)/$2" | bc
 }
 
-
+summary=false
 [ -n "${1-}" ] && [[ $1 == '-h' || $1 == '--help' ]] && usage 0
+[ -n "${1-}" ] && [ "$1" == "-s" ] && { summary=true; shift; }
 [ $# -lt 2 ] && usage 1
 caselist=$1 && [ -f "$caselist" ] || { echo "'$caselist' doesn't exist"; usage 1; }
 shift && filepatterns="$@"
 
 num_completed=0
-printf "%-10s %-20s\n" Case Status
+$summary || printf "%-10s %-20s\n" Case Status
 while read case; do
     if completed $case $filepatterns; then 
         num_completed=$((num_completed+1))
-        printf "%-10s done\n" $case
+        $summary || printf "%-10s done\n" $case
     else
-        printf "%-10s incomplete\n" $case
+        $summary || printf "%-10s incomplete\n" $case
     fi
 done < $caselist
 
 total=$(wc -l < $caselist)
-echo -e "\nProgress:"
-printf "%i/%s (%s%%)\n" $num_completed $total $(ratio_percent $num_completed $total)
+$summary || echo -e "\nProgress:"
+printf "%i/%s  %s%%\n" $num_completed $total $(ratio_percent $num_completed $total)
