f42dfd30c9edd8d48384eeca947f87b44eddebd2
reckbo <reckbo@bwh.harvard.edu>
2014-10-15 21:03:07 -0400

    ENH: Make mask.sh accept any file format inputs

--- mask.sh
+++ mask.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -eu
+SCRIPT=$(readlink -m $(type -p $0))
+SCRIPTDIR=$(dirname $SCRIPT)
+source "$SCRIPTDIR/util.sh"
 
 usage() {
     cat <<EOF
-Usage :
+    Usage :
 
     mask.sh <img> <mask> <out>
 
-Masks <img> (including DWI's) with <mask>.  Both must be in either nrrd or nifti format.
+    Masks <img> (including DWI's) with <mask>.  Both must be in either nrrd or nifti format.
 EOF
-}
-
-isnrrd()
-{
-    [[ $1 = *nrrd || $1 = *nhdr ]]
-}
-
-isnifti()
-{
-    [[ $1 = *nii || $1 = *nii.gz ]]
 }
 
 isdwi() {
@@ -32,15 +24,15 @@ mask_dwi() {
     local mask=$2
     local out=$3
     tmpdir=$(mktemp -d)
-    unu dice -i $dwi -a 3 -o $tmpdir/dwi
-    for slice in $tmpdir/dwi*nrrd; do 
-        unu 3op ifelse $mask $slice 0 -o ${slice%.nrrd}-masked.nrrd
+    run unu dice -i $dwi -a 3 -o $tmpdir/dwi
+    for slice in $tmpdir/dwi*nrrd; do
+        run unu 3op ifelse $mask $slice 0 -o ${slice%.nrrd}-masked.nrrd
     done
-    unu join -a 3 -i $tmpdir/*-masked.nrrd | unu data - > $tmpdir/tmpdwi.raw.gz
-    unu head $dwi > $tmpdir/tmpdwi.nhdr
-    sed "s/data file.*$/data file: tmpdwi\.raw\.gz/" -i $tmpdir/tmpdwi.nhdr
-    unu save -e gzip -f nrrd -i $tmpdir/tmpdwi.nhdr -o $out
-    rm -rf $tmpdir
+    run "unu join -a 3 -i $tmpdir/*-masked.nrrd | unu data - > $tmpdir/tmpdwi.raw.gz"
+    run "unu head $dwi > $tmpdir/tmpdwi.nhdr"
+    run "sed "s/data file.*$/data file: tmpdwi\.raw\.gz/" -i $tmpdir/tmpdwi.nhdr"
+    run "unu save -e gzip -f nrrd -i $tmpdir/tmpdwi.nhdr -o $out"
+    run rm -rf $tmpdir
 }
 
 [ -n "${1-}" ] && [[ $1 == "-h" || $1 == "--help" ]] && { usage; exit 0; }
@@ -50,20 +42,19 @@ img=$1
 mask=$2
 out=$3
 
-if  isnrrd $img && isnrrd $mask && isnrrd $out; then
-    if isdwi $img; then
-        mask_dwi $img $mask $out
-    else
-        unu 3op ifelse $mask $img 0 -w 1 | unu save -e gzip -f nrrd -o $out
-    fi
-elif isnifti $img && isnifti $mask && isnifti $out; then
-    echo "Assuming label number in the mask is 1, if not the result will be wrong!"
-    fslmaths $img -mul $mask $out
+tmp=$(mktemp -d)
+tmpimg=$tmp/$(base $img).nrrd
+tmpmask=$tmp/$(base $mask).nrrd
+tmpout=$tmp/$(base $out).nrrd
+
+ConvertBetweenFileFormats "$img" "$tmpimg"
+ConvertBetweenFileFormats "$mask" "$tmpmask"
+
+if isdwi $tmpimg; then
+    mask_dwi $tmpimg $tmpmask $tmpout
 else
-    echo $img 
-    echo $mask 
-    echo $out 
-    echo "must all be in same format, either nrrd/nhdr or nii/nii.gz"
-    exit 1
+    run "unu 3op ifelse $tmpmask $tmpimg 0 -w 1 | unu save -e gzip -f nrrd -o $tmpout"
 fi
-echo "Made $out"
+
+ConvertBetweenFileFormats "$tmpout" "$out"
+log_success "Made $out"
