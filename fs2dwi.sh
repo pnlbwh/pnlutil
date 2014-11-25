734f56f8e9ecb29f1cc4ee9a59dd67b9e451b515
reckbo <reckbo@bwh.harvard.edu>
2014-11-24 20:15:49 -0500

    *fs2dwi.sh: check args up front, add more log messages

--- fs2dwi.sh
+++ fs2dwi.sh
@@ -16,22 +16,23 @@ where <dwi> and <dwi_mask> are nrrd/nhdr files
 [ -n "${1-}" ] && [[ $1 == "-h" || $1 == "--help" ]] && usage 0
 [ $# -ne 4 ] && usage 1
 
+check_set_vars FREESURFER_HOME ANTSPATH ANTSSRC
+export SUBJECTS_DIR=
+
 input_vars="dwi dwi_mask mri output_dir"
 read -r $input_vars <<<"$@"
 get_remotes ${input_vars% *}
 
-check_set_vars ANTSPATH FREESURFER_HOME
-export SUBJECTS_DIR=
-
 log "Make and change to output directory"
-run mkdir $output_dir || { log_fail "$output_dir already exists, delete it or choose another output folder name"; exit 1; }
+run mkdir $output_dir || { log_error "$output_dir already exists, delete it or choose another output folder name"; exit 1; }
 run pushd $output_dir
 
-log "Create brain.nii.gz and wmparc.nii.gz from their mgz versions"
+log "Create 'brain.nii.gz' and 'wmparc.nii.gz' from their mgz versions"
 #$fsbin/mri_convert -rt nearest --in_type mgz --out_type nii --out_orientation LPI $mri/wmparc.mgz $mri/wmparc.nii.gz
 #$fsbin/mri_convert -rt nearest --in_type mgz --out_type nii --out_orientation LPI $mri/brain.mgz $mri/brain.nii.gz
 run $FREESURFER_HOME/bin/mri_vol2vol --mov $mri/brain.mgz --targ $mri/brain.mgz --regheader --o brain.nii.gz
 run $FREESURFER_HOME/bin//mri_label2vol --seg $mri/wmparc.mgz --temp $mri/brain.mgz --o wmparc.nii.gz --regheader $mri/wmparc.mgz
+log_success "Made 'brain.nii.gz' and 'wmparc.nii.gz'"
 
 log "Create masked baseline"
 bse=$(basename "$dwi")
@@ -51,12 +52,13 @@ run mv brain-to-bse-1mm-deformed.nii.gz brain-in-bse-1mm.nii.gz
 
 log "Apply warp to wmparc.nii.gz to create wmparc-in-bse-1mm.nii.gz"
 run $ANTSPATH/antsApplyTransforms -d 3 -i wmparc.nii.gz -o wmparc-in-bse-1mm.nrrd -r "$maskedbse1mm" -n NearestNeighbor -t brain-to-bse-1mm-Warp.nii.gz brain-to-bse-1mm-Affine.txt 
-log_success "Created 'wmparc-in-bse-1mm.nii.gz'"
+log_success "Made 'wmparc-in-bse-1mm.nii.gz'"
 
 log "Downsample wmparc-in-bse-1mm.nii.gz to DWI's resolution"
 new_size=$(unu head $maskedbse | grep "sizes:" | sed 's/sizes:\s*//')
 run "unu resample -k cheap -s $new_size -i wmparc-in-bse-1mm.nrrd | unu save -e gzip -f nrrd -o wmparc-in-bse.nrrd"
 ConvertBetweenFileFormats wmparc-in-bse.nrrd wmparc-in-bse.nrrd short
+log_success "Downsampled wmparc: 'wmparc-in-bse.nrrd'"
 
 popd
 log_success "Made '$(readlink -f "$output_dir"/wmparc-in-bse.nrrd)'"
