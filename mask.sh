e5aa8111b38a86cf6ac7a87442258ecd549f343e
reckbo <reckbo@bwh.harvard.edu>
2014-12-01 06:57:52 -0500

    *Improved mask.sh

--- mask.sh
+++ mask.sh
@@ -2,59 +2,39 @@
 
 set -eu
 SCRIPT=$(readlink -m $(type -p $0))
-SCRIPTDIR=$(dirname $SCRIPT)
+SCRIPTDIR=${SCRIPT%/*}
 source "$SCRIPTDIR/util.sh"
 
 usage() {
-    cat <<EOF
-    Usage :
+echo -e "Usage:
 
     mask.sh <img> <mask> <out>
 
-    Masks <img> (including DWI's) with <mask>.
-EOF
+Masks <img> (including DWI's) with <mask>.
+"
 }
 
 isdwi() {
     unu head $1 | egrep -q 'dimension.*4'
 }
 
-mask_dwi() {
-    local dwi=$1
-    local mask=$2
-    local out=$3
-    tmpdir=$(mktemp -d)
-    run unu dice -i $dwi -a 3 -o $tmpdir/dwi
-    for slice in $tmpdir/dwi*nrrd; do
-        run unu 3op ifelse $mask $slice 0 -o ${slice%.nrrd}-masked.nrrd
-    done
-    run "unu join -a 3 -i $tmpdir/*-masked.nrrd | unu data - > $tmpdir/tmpdwi.raw.gz"
-    run "unu head $dwi > $tmpdir/tmpdwi.nhdr"
-    run "sed "s/data file.*$/data file: tmpdwi\.raw\.gz/" -i $tmpdir/tmpdwi.nhdr"
-    run "unu save -e gzip -f nrrd -i $tmpdir/tmpdwi.nhdr -o $out"
-    run rm -rf $tmpdir
-}
+[ -n "${1-}" ] && [[ $1 == "-h" || $1 == "--help" ]] && { usage; exit 1; }
+[ "$#" -ne 3 ] && { usage; exit 1; }
+read -r img mask out <<<"$@"
 
-[ -n "${1-}" ] && [[ $1 == "-h" || $1 == "--help" ]] && { usage; exit 0; }
-[ $# -lt 3 ]  && { usage >&2; exit 1; }
-
-img=$1
-mask=$2
-out=$3
-
-tmp=$(mktemp -d)
-tmpimg=$tmp/$(base $img).nrrd
-tmpmask=$tmp/$(base $mask).nrrd
-tmpout=$tmp/$(base $out).nrrd
-
-ConvertBetweenFileFormats "$img" "$tmpimg"
-ConvertBetweenFileFormats "$mask" "$tmpmask"
-
-if isdwi $tmpimg; then
-    mask_dwi $tmpimg $tmpmask $tmpout
-else
-    run "unu 3op ifelse $tmpmask $tmpimg 0 -w 1 | unu save -e gzip -f nrrd -o $tmpout"
+if [[ $img == *.nhdr || $img == *.nrrd ]] && isdwi $img; then
+    run "unu 3op ifelse -w 1 $mask $img 0 | unu save -e gzip -f nrrd -o $out"
+    log_success "Made '$out'"
+    exit 0
 fi
 
-ConvertBetweenFileFormats "$tmpout" "$out"
-log_success "Made $out"
+tmp=$(mktemp -d)
+tmpimg=$tmp/${img##*/}.nrrd
+tmpmask=$tmp/${mask##*/}.nrrd
+tmpout=$tmp/${out##*/}.nrrd
+
+run ConvertBetweenFileFormats "$img" "$tmpimg" >/dev/null
+run ConvertBetweenFileFormats "$mask" "$tmpmask" >/dev/null
+run "unu 3op ifelse -w 1 $tmpmask $tmpimg 0 | unu save -e gzip -f nrrd -o $tmpout"
+run ConvertBetweenFileFormats "$tmpout" "$out" >/dev/null
+log_success "Made '$out'"
