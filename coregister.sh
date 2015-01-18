d38e66ab58663e44c401b1a37ed98d09cb6251be
reckbo <reckbo@bwh.harvard.edu>
2015-01-18 18:25:53 -0500

    +coregister.sh

---
+++ coregister.sh
@@ -0,0 +1,25 @@
+#!/bin/bassh -eu
+
+SCRIPT=$(readlink -m $(type -p $0))
+SCRIPTDIR=${SCRIPT%/*}
+source "$SCRIPTDIR/util.sh"
+
+t2=$1
+t1=$2
+out=$3
+
+tmpdir=$(mktemp -d)
+t2nii="$tmpdir/t2.nii.gz"
+t1nii="$tmpdir/t1.nii.gz"
+outnii="$tmpdir/t2-co.nii.gz"
+
+log "Rigid register '$t2' to '$t1' to make '$out'"
+run ConvertBetweenFileFormats $t1 $t1nii
+run ConvertBetweenFileFormats $t2 $t2nii
+# sometimes we need to swap t2 dims, see coregisterAndMask.py
+
+run flirt -dof 6 -in $t2nii -ref $t1nii -out $outnii
+run ConvertBetweenFileFormats $outnii $out
+
+log_success "Made '$out'"
+rm -rf "$tmpdir"
