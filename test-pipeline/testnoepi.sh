16be4cc7c203a26bf353478d20e027471aaa3d41
reckbo <reckbo@bwh.harvard.edu>
2015-04-16 06:33:44 -0400

    *Move testpipeline/ out of tests/

--- test-pipeline/testnoepi.sh
+++ test-pipeline/testnoepi.sh
@@ -2,7 +2,7 @@
 
 dir=noepi
 [ $# -eq 0 ]  || dir=$1
-../../mkpipeline.sh $dir
+../mkpipeline.sh $dir
 cp SetUpData_config.sh $dir/SetUpData_config.sh
 cp training*.txt $dir
 cd $dir && redo 01010.all
