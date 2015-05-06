75dd975249cc87ef4dc33c81b5cd24fa5ae1d2cd
reckbo <reckbo@bwh.harvard.edu>
2015-05-06 19:57:13 -0400

    BUG/STY:fix bse.sh and add better doc

--- bse.sh
+++ bse.sh
@@ -11,12 +11,14 @@ directions comprise the last (slowest index) axis.
 
 Usage:
         
-    ${0##*/} <dwi> <out>
+    ${0##*/} [-m <dwimask>] <dwi> <out>
 
-where <dwi> must be a nrrd volume."
+    where <dwi> must be a nrrd volume (nrrd/nhdr). If <dwimask>
+    is given then the baseline is also masked."
 }
 
 dwimask=""
+[ $# -gt 0 ] || { usage; exit 1; }
 while getopts "hm:" flag; do
     case "$flag" in
         h) usage 1;;
@@ -25,12 +27,14 @@ while getopts "hm:" flag; do
 done
 shift $((OPTIND-1))
 read -r dwi out <<<"$@"
+checkexists dwi
+[ -z "$dwimask" ] || checkexists dwimask
 
 regex="DWMRI_gradient_\([0-9]*\):= *0\(\.0*\)\{0,1\} 0\(\.0*\)\{0,1\} 0\(\.0*\)\{0,1\}"
-dir=$(unu head $dwi | sed -n "s|$regex|\1|p" | head -n 1)
-log "Found baseline at gradient direction '$dir'"
+direction=$(unu head $dwi | sed -n "s|$regex|\1|p" | head -n 1)
+log "Found baseline at gradient direction '$direction'"
 if [ -n "$dwimask" ]; then
-    run "unu slice -a 3 -p $dir -i $dwi | unu 3op ifelse -w 1 $dwimask - 0 -o $out"
+    run "unu slice -a 3 -p $direction -i $dwi | unu 3op ifelse -w 1 $dwimask - 0 -o $out"
 else
-    run unu slice -a 3 -p $dir -i "$dir" -o "$out"
+    run unu slice -a 3 -p $direction -i "$dwi" -o "$out"
 fi
