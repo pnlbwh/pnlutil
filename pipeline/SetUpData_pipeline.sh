ced236275a725d0fd246c441c7f7eaaf6d51d23d
reckbo <reckbo@bwh.harvard.edu>
2015-07-20 18:12:14 +1000

    STY: make SetUpData_pipeline.sh more readable

--- pipeline/SetUpData_pipeline.sh
+++ pipeline/SetUpData_pipeline.sh
@@ -1,21 +1,36 @@
 diff=$base/$case/diff
 strct=$base/$case/strct
+status_vars_extra=""
+status_vars_epi=""
 
-# Axis align and center
+# ==========================================================
+# Axis align and center DWI
 dwixc_dwi=$dwiraw
 dwixc=$diff/$case.dwixc.nrrd
+# ==========================================================
+
+
+# ==========================================================
+# Axis align and center T1
 t1xc_t1=$t1align
 t1xc=$strct/$case.t1xc.nrrd
+# ==========================================================
 
-# Atlas mask
+
+# ==========================================================
+# MABS (atlas) mask
 # Inputs
 atlas_target=$t1xc
 atlas_trainingstructs=${base}/trainingt1s.txt
 atlas_traininglabels=${base}/trainingmasks.txt
 # Output
 t1atlasmask=$strct/$case.t1atlasmask.nrrd
+# ==========================================================
 
-status_vars_extra=""
+
+# ==========================================================
+# Freesurfer
+# Inputs
 if [ -n "${ATLASMASK_EDIT:-}" ]; then
     t1atlasmask_edit=$strct/$case.t1atlasmask.edited.nrrd
     fs_mask=$t1atlasmask_edit
@@ -23,85 +38,106 @@ if [ -n "${ATLASMASK_EDIT:-}" ]; then
 else
     fs_mask=$t1atlasmask
 fi
-
-# Freesurfer
-# Inputs
 fs_t1=$t1xc
-#fs_mask=$t1atlasmask  # set above
 # Output
 fs=$strct/$case.freesurfer
+# ==========================================================
 
-# DWI preprocessing
+
+# ==========================================================
+# DWI eddy current correction
 # Inputs:
 dwied_dwi=$dwixc
 # Output
 dwied=$diff/$case.dwi-Ed.nrrd  
+# ==========================================================
 
+
+# ==========================================================
 # DWI mask
 # Inputs:
 dwibetmask_dwi=$dwied
 # Output
 dwibetmask=$diff/$case.dwibetmask.nrrd  
+[ ! -n "${DWIMASK_EDIT:-}" ] || dwibetmask=$diff/$case.dwibetmask.edited.nrrd  
+# ==========================================================
 
-if [ -n "${DWIMASK_EDIT:-}" ]; then
-    dwibetmask_edit=$diff/$case.dwibetmask.edited.nrrd  
-    dwiepi_dwimask=$dwibetmask_edit
-    ukf_dwimask=$dwibetmask_edit
 
-    status_vars_extra="$status_vars_extra dwibetmask_edit"
-else
-    dwiepi_dwimask=$dwibetmask
-    ukf_dwimask=$dwibetmask
-fi
+#if [ -n "${DWIMASK_EDIT:-}" ]; then
+    #dwibetmask_edit=$diff/$case.dwibetmask.edited.nrrd  
+    #dwiepi_dwimask=$dwibetmask_edit
+    #ukf_dwimask=$dwibetmask_edit
 
-status_vars_epi=""
+    #status_vars_extra="$status_vars_extra dwibetmask_edit"
+#else
+    #dwiepi_dwimask=$dwibetmask
+    #ukf_dwimask=$dwibetmask
+#fi
+
 if $EPICORRECTION; then
     status_vars_epi="t2 dwiepi dwiepimask"
+    # ==========================================================
     # DWI epi correction
     # Inputs
     dwiepi_dwi=$dwied
-    #dwiepi_dwimask=$dwibetmask # set above
+    dwiepi_dwimask=$dwibetmask 
     dwiepi_t2=$t2
     # Output
     dwiepi=$diff/$case.dwi-epi.nrrd
+    # ==========================================================
 
+    # ==========================================================
     # DWI epi mask (for ukftractography seed map)
     # Input
     dwiepimask_dwi=$dwiepi
     # Output
     dwiepimask=$diff/$case.dwi-epi-mask.nrrd
+    # ==========================================================
 
+    # ==========================================================
     # UKF
     # Inputs
     ukf_dwi=$dwiepi
-    #ukf_dwimask=$dwiepimask # set above
+    ukf_dwimask=$dwiepimask 
     # Output
     ukf=$diff/$case.ukf_2T.vtk.gz
+    # ==========================================================
 
+    # ==========================================================
     # Freesurfer to DWI registration
     # Inputs
     fsindwi_dwi=$dwiepi
     fsindwi_dwimask=$dwiepimask
+    # ==========================================================
 else
+    # ==========================================================
     # UKF
     # Inputs
     ukf_dwi=$dwied
-    #ukf_dwimask=$dwibetmask # set above
+    ukf_dwimask=$dwibetmask 
     # Output
     ukf=$diff/$case.ukf_2T.vtk.gz
+    # ==========================================================
 
+    # ==========================================================
     # Freesurfer to DWI registration
     # Inputs
     fsindwi_dwi=$dwied
     fsindwi_dwimask=$dwibetmask
+    # ==========================================================
 fi
 
+
+# ==========================================================
 # Freesurfer to DWI registration
 # Inputs
 fsindwi_fssubjectdir=$fs
 # Output
 fsindwi=$diff/$case.fsindwi.nrrd
+# ==========================================================
 
+
+# ==========================================================
 # WMQL
 # Inputs
 wmqltracts_tractography=$ukf
@@ -109,19 +145,27 @@ wmqltracts_wmparc=$fsindwi
 wmqltracts_query=wmql_query.txt
 # Output
 wmqltracts=$diff/$case.wmqltracts
+# ==========================================================
 
+# ==========================================================
 # WMQL tract measures
 # Inputs
 tractmeasures_tracts=$wmqltracts
 # Output
 tractmeasures=$diff/$case.tractmeasures.csv
+# ==========================================================
 
+
+# ==========================================================
 # WMQL tract volumes
 # Inputs
 tractvols_tracts=$wmqltracts
 # Output
 tractvols=$diff/$case.tractvols.csv
+# ==========================================================
 
+
+# used by script 'logstatus', which writes 'statuslog.csv'
 status_vars="\
     t1align \
     t1xc \
