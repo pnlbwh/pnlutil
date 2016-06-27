e73b6ec0f12f1df3163ba56c3e99a21651dfe9ad
reckbo <reckbo@bwh.harvard.edu>
2016-06-26 20:45:33 -0400

    BUG: add measuretracts to addpaths.sh

--- install-software/addpaths.sh
+++ install-software/addpaths.sh
@@ -4,19 +4,13 @@
 base=$(readlink -m ${BASH_SOURCE[0]}) && base=${base%/*}
 
 redo=$base/redo
-
 brainstools=$base/BRAINSTools-build/bin
-
 #ants=$base/BRAINSTools-build/bin
 antssrc=$base/BRAINSTools-build/ANTs
-
 pnlutil=$base/../scripts-all
-
-#tract_querier # installed to your python installation, make sure bin folder is on path
-
 ukf=$base/UKFTractography-build/UKFTractography-build/ukf/bin/
+measuretracts=$base/measuretracts
 
-export PATH=$redo:$brainstools:$ants:$antssrc:$pnlutil:$ukf:$PATH
-
+export PATH=$redo:$brainstools:$ants:$antssrc:$pnlutil:$ukf:$measuretracts:$PATH
 export ANTSPATH=$brainstools
 export ANTSSRC=$antssrc
