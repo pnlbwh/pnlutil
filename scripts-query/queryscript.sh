f5d4a783d4fe9913edc7b9252ce34ec9bea58dba
reckbo <reckbo@bwh.harvard.edu>
2017-02-05 23:40:35 +0000

    ENH: make query scripts look in _data for SetUpData.sh

--- scripts-query/queryscript.sh
+++ scripts-query/queryscript.sh
@@ -2,25 +2,25 @@
 # Query script functions/helpers
 # Sourced by query scripts
 
-join() { 
-    local IFS="$1"; shift; echo "$*"; 
+join() {
+    local IFS="$1"; shift; echo "$*";
     #delim=$1; shift;
     #printf "%s%s\n" "$delim" "$*"
 }
 
 setcases() {
-    if [ -n "${argcases-}" ]; then 
+    if [ -n "${argcases-}" ]; then
         cases=$argcases
-    elif [ -n "${argcaselist-}" ]; then 
+    elif [ -n "${argcaselist-}" ]; then
         [ -f "$argcaselist" ] || { echo "'$argcaselist' doesn't exist."; exit 1; }
         cases=$(cat "$argcaselist" | awk '{print $1}')
     else
-        case=000 && source SetUpData.sh
-        if [ ! -n "${caselist-}" ] && [ ! -n "${cases-}" ]; then 
+        case=000 && source $SetUpData
+        if [ ! -n "${caselist-}" ] && [ ! -n "${cases-}" ]; then
             echo -e "Set variable 'caselist' (a text file) or 'cases' (a string of case ids) in 'SetUpData.sh', \nor pass them as arguments on the commandline."
             exit 1
         fi
-        if [ -n "${caselist-}" ] && [ -n "${cases-}" ]; then 
+        if [ -n "${caselist-}" ] && [ -n "${cases-}" ]; then
             echo "You have set both 'caselist' and 'cases' in your SetUpData.sh, just set just one."
             exit 1
         fi
@@ -31,13 +31,24 @@ setcases() {
     fi
 }
 
+setSetUpData() {
+    if [ -f SetUpData.sh ]; then
+        SetUpData="SetUpData.sh"
+    elif [ -f "_data/SetUpData.sh" ]; then
+        SetUpData="_data/SetUpData.sh"
+    else
+        echo "Run in directory with SetUpData.sh or _data/SetUpData.sh."
+        exit 1;
+    fi
+}
+
 delim=","
 printcaseids=false
 # Parse args
 while getopts "hd:cl:f:" flag; do
 case "$flag" in
     h) usage; exit;;
-    d) delim=$OPTARG;; 
+    d) delim=$OPTARG;;
     l) argcases=$OPTARG;;
     c) printcaseids=true;;
     f) argcaselist=$OPTARG;;
@@ -48,8 +59,10 @@ shift $((OPTIND-1))
 [ $# -gt 0 ] || { usage; }
 # Get positional arguments (vars) and make sure they are set in SetUpData.sh
 vars=$@
-[ -f SetUpData.sh ] || { echo "Run in directory with SetUpData.sh."; usage; exit 1; }
-case=000 && source SetUpData.sh
+
+setSetUpData
+
+case=000 && source "$SetUpData"
 for var in $vars; do
     [ -n "${!var-}" ] || { echo "Set '$var' in SetUpData.sh first."; usage; exit 1; }
 done
